62689426f956bf40d7027988de7265d8cbfe6ef9
Michael <Michael@DESKTOP-61L2M0C>
2023-10-18 12:18:30 -0300

    Adicionar arquivos de projeto.

---
+++ MyFirstApi/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstApi.Models;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyFirstApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // GET: api/<UserController>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            List<User> users = new List<User>();
+            users.Add(new User() {Id = 1, Name = "Michael", Email = "michael.steam@outlook.com" } );
+            return users;       
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            User user = new User() { Id = 1, Name = "Michael", Email = "michael.steam@outlook.com" };
+            return user;
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public void Post([FromBody] User user)
+        {
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] User user)
+        {
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
